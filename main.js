c3aa502b3be83f5c564a1bd97b88ec2976335083
xpertitorg <103072899+xpertitorg@users.noreply.github.com>
2025-07-24 22:39:20 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,492 @@
+const produkte = [
+  {
+    "artikelnummer": "200",
+    "name": "Ware",
+    "beschreibung": "xDomain\r\n- Verwaltung und Pflege der Domain\r\n- SSL Zertifizierung*\r\n- Kosten pro…",
+    "preis": 19.9,
+    "kategorie": "2 - Managed Services",
+    "farbe": "#DBEAFE"
+  },
+  {
+    "artikelnummer": "210",
+    "name": "Ware",
+    "beschreibung": "xProtect Standard\n- Endpoint Protection für Clients und Server\n- Umfassender Sch…",
+    "preis": 3.99,
+    "kategorie": "2 - Managed Services",
+    "farbe": "#DBEAFE"
+  },
+  {
+    "artikelnummer": "211",
+    "name": "Ware",
+    "beschreibung": "xProtect Exclusive\n- Endpoint Protection für Clients und Server\n- Umfassender Sc…",
+    "preis": 9.0,
+    "kategorie": "2 - Managed Services",
+    "farbe": "#DBEAFE"
+  },
+  {
+    "artikelnummer": "212",
+    "name": "Ware",
+    "beschreibung": "xProtect Premium\n- Endpoint Protection für Clients und Server\n- Umfassender Schu…",
+    "preis": 19.9,
+    "kategorie": "2 - Managed Services",
+    "farbe": "#DBEAFE"
+  },
+  {
+    "artikelnummer": "220",
+    "name": "Ware",
+    "beschreibung": "xMailArchiv\n- Revisionssichere E-Mail Archivierung gemäß § 146 AO und § 239 HGB\n…",
+    "preis": 9.0,
+    "kategorie": "2 - Managed Services",
+    "farbe": "#DBEAFE"
+  },
+  {
+    "artikelnummer": "222",
+    "name": "Ware",
+    "beschreibung": "xTicket:\r\n\r\n- Wartung, Pflege und Hosting des Ticketsystems\r\n- Updatemanagement …",
+    "preis": 30.0,
+    "kategorie": "2 - Managed Services",
+    "farbe": "#DBEAFE"
+  },
+  {
+    "artikelnummer": "300",
+    "name": "Ware",
+    "beschreibung": "Microsoft 365 Apps for Business\r\n- Word, Excel, PowerPoint und Outlook\r\n- OneDri…",
+    "preis": 11.25,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "301",
+    "name": "Ware",
+    "beschreibung": "Exchange Online Plan 1\r\n- Exchange Postfach mit 50 GB Speicherplatz\r\n- Kosten pr…",
+    "preis": 6.4,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "302",
+    "name": "Ware",
+    "beschreibung": "Microsoft 365 Business Basic\r\nWeb- und Mobile-Versionen von:\r\n- Word, Excel, Pow…",
+    "preis": 8.9,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "303",
+    "name": "Ware",
+    "beschreibung": "Microsoft 365 Business Standard\r\n- Word, Excel, PowerPoint und Outlook\r\n- Instal…",
+    "preis": 15.9,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "304",
+    "name": "Ware",
+    "beschreibung": "Microsoft 365 Business Premium\r\n- Word, Excel, PowerPoint und Outlook\r\n- Install…",
+    "preis": 22.1,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "305",
+    "name": "Ware",
+    "beschreibung": "Microsoft 365 Copilot",
+    "preis": 28.1,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "308",
+    "name": "Ware",
+    "beschreibung": "Microsoft Teams-Räume Pro\r\n- Nahtloser Übergang zu Hybridarbeit mit KI-gesteuert…",
+    "preis": 44.88,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "309",
+    "name": "Ware",
+    "beschreibung": "Exchange Premium\r\n- Exchange Postfach mit 50 GB Speicherplatz\r\n- Kosten pro Benu…",
+    "preis": 9.4,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "310",
+    "name": "Ware",
+    "beschreibung": "xBackup Basic für Microsoft 365\n- 1 Terrabyte Speicherplatz (erweiterbar)\n- Sich…",
+    "preis": 60.0,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "311",
+    "name": "Ware",
+    "beschreibung": "xBackup Premium für Microsoft 365\n- unbegrenzter Speicherplatz\n- Sicherung vom d…",
+    "preis": 10.0,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "311",
+    "name": "Ware",
+    "beschreibung": "xBackup Premium für Microsoft 365\n- unbegrenzter Speicherplatz\n- Sicherung vom d…",
+    "preis": 10.0,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "312",
+    "name": "Ware",
+    "beschreibung": "xBackup Premium Workstation\r\n- für Win/Mac OS Clients\r\n- Sichern und Wiederherst…",
+    "preis": 4.99,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "312",
+    "name": "Ware",
+    "beschreibung": "xBackup Premium Workstations\r\n- für Win/Mac OS Clients\r\n- Sichern und Wiederhers…",
+    "preis": 4.99,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "314",
+    "name": "Ware",
+    "beschreibung": "xBackup Premium Servers\r\n- für Windows-Server\r\n- Sichern und Wiederherstellen vo…",
+    "preis": 14.9,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "315",
+    "name": "Ware",
+    "beschreibung": "xBackup - 1TB Cloud-Storage\r\n\r\n- Speicherpool für Backups\r\n- Monitoring der Back…",
+    "preis": 49.9,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "316",
+    "name": "Ware",
+    "beschreibung": "xBackup - 2TB Cloud-Storage\r\n\r\n- Speicherpool für Backups\r\n- Monatliche Kosten\r\n…",
+    "preis": 79.9,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "316",
+    "name": "Ware",
+    "beschreibung": "xBackup - 5TB Cloud-Storage\r\n\r\n- Speicherpool für Backups\r\n- Monatliche Kosten\r\n…",
+    "preis": 109.9,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "316",
+    "name": "Ware",
+    "beschreibung": "xBackup -10TB Cloud-Storage\r\n\r\n- Speicherpool für Backups\r\n- Monatliche Kosten\r\n…",
+    "preis": 149.9,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "316",
+    "name": "Ware",
+    "beschreibung": "xBackup -10TB Cloud-Storage\r\n\r\n- Speicherpool für Backups\r\n- Monitoring der Back…",
+    "preis": 149.9,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "316",
+    "name": "Ware",
+    "beschreibung": "xBackup - 5TB Cloud-Storage\r\n\r\n- Speicherpool für Backups\r\n- Monitoring der Back…",
+    "preis": 109.9,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "316",
+    "name": "Ware",
+    "beschreibung": "xBackup - 2TB Cloud-Storage\r\n\r\n- Speicherpool für Backups\r\n- Monitoring der Back…",
+    "preis": 79.9,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "320",
+    "name": "Ware",
+    "beschreibung": "xVoIP User\r\n- Uneingeschränkte Nutzung der Cloud Telefonie\r\n- Kosten pro Benutze…",
+    "preis": 7.0,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "321",
+    "name": "Ware",
+    "beschreibung": "xVoIP Server 4SC\r\n- Cloud Telefonanlage mit 4 gleichzeitigen Gesprächen\r\n- Updat…",
+    "preis": 29.0,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "322",
+    "name": "Ware",
+    "beschreibung": "xVoIP Server 8SC\r\n- Cloud Telefonanlage mit 8 gleichzeitigen Gesprächen\r\n- Updat…",
+    "preis": 39.0,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "329",
+    "name": "Ware",
+    "beschreibung": "xVoIP Upgrade-Lizenz\r\n\r\n- Immer die aktuelle Version der Cloud Telefonanlage.\r\n-…",
+    "preis": 199.0,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "330",
+    "name": "Ware",
+    "beschreibung": "xCloud-File\n- 2TB Speicherplatz*\n- Online Zugriff\n- Für Mac OS und Windows OS\n- …",
+    "preis": 14.9,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "340",
+    "name": "Ware",
+    "beschreibung": "xDrive 1TB*\n\n- Dezentrale Datensicherung in einem deutschen Serverzentrum (Hochs…",
+    "preis": 39.9,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "350",
+    "name": "Ware",
+    "beschreibung": "xWeb\n- Webspace für die Ablage von Webseiten\n- SSL Zertifizierung\n- Kosten pro M…",
+    "preis": 21.67,
+    "kategorie": "3 - Cloud Services",
+    "farbe": "#DCFCE7"
+  },
+  {
+    "artikelnummer": "500",
+    "name": "Ware",
+    "beschreibung": "SLA 2 Standard\n\n- 2 Stunden Reaktionszeit innerhalb der Regelarbeitszeit.\n- Proa…",
+    "preis": 3000.0,
+    "kategorie": "5 - Support-Verträge",
+    "farbe": "#FFEDD5"
+  },
+  {
+    "artikelnummer": "502",
+    "name": "Ware",
+    "beschreibung": "SLA 2 Premium\n\n- 2 Stunden Reaktionszeit 24/7.\n- Proaktives Netzwerkmonitoring (…",
+    "preis": 5500.0,
+    "kategorie": "5 - Support-Verträge",
+    "farbe": "#FFEDD5"
+  },
+  {
+    "artikelnummer": "510",
+    "name": "Ware",
+    "beschreibung": "SLA 4 Standard\r\n\r\n- 4 Stunden Reaktionszeit innerhalb der Regelarbeitszeit.\r\n- P…",
+    "preis": 1500.0,
+    "kategorie": "5 - Support-Verträge",
+    "farbe": "#FFEDD5"
+  },
+  {
+    "artikelnummer": "512",
+    "name": "Ware",
+    "beschreibung": "SLA 4 Premium\r\n\r\n- 4 Stunden Reaktionszeit 24/7.\r\n- Proaktives Netzwerkmonitorin…",
+    "preis": 3500.0,
+    "kategorie": "5 - Support-Verträge",
+    "farbe": "#FFEDD5"
+  },
+  {
+    "artikelnummer": "520",
+    "name": "Ware",
+    "beschreibung": "SLA 6 Standard\r\n\r\n- 6 Stunden Reaktionszeit innerhalb der Regelarbeitszeit.\r\n- P…",
+    "preis": 900.0,
+    "kategorie": "5 - Support-Verträge",
+    "farbe": "#FFEDD5"
+  },
+  {
+    "artikelnummer": "522",
+    "name": "Ware",
+    "beschreibung": "SLA 6 Premium\r\n\r\n- 6 Stunden Reaktionszeit 24/7.\r\n- Proaktives Netzwerkmonitorin…",
+    "preis": 2300.0,
+    "kategorie": "5 - Support-Verträge",
+    "farbe": "#FFEDD5"
+  },
+  {
+    "artikelnummer": "530",
+    "name": "Ware",
+    "beschreibung": "SLA 8 Standard\r\n\r\n- 8 Stunden Reaktionszeit innerhalb der Regelarbeitszeit.\r\n- P…",
+    "preis": 500.0,
+    "kategorie": "5 - Support-Verträge",
+    "farbe": "#FFEDD5"
+  },
+  {
+    "artikelnummer": "532",
+    "name": "Ware",
+    "beschreibung": "SLA 8 Premium\r\n\r\n- 8 Stunden Reaktionszeit 24/7.\r\n- Proaktives Netzwerkmonitorin…",
+    "preis": 1900.0,
+    "kategorie": "5 - Support-Verträge",
+    "farbe": "#FFEDD5"
+  },
+  {
+    "artikelnummer": "590",
+    "name": "Ware",
+    "beschreibung": "Power User \r\n\r\n•\tMicrosoft 365 Business Standard Account inklusive Mail-Archivie…",
+    "preis": 59.0,
+    "kategorie": "5 - Support-Verträge",
+    "farbe": "#FFEDD5"
+  },
+  {
+    "artikelnummer": "591",
+    "name": "Ware",
+    "beschreibung": "Standard User \r\n\r\n•\tMicrosoft 365 Business Standard Account inklusive Mail-Archi…",
+    "preis": 34.0,
+    "kategorie": "5 - Support-Verträge",
+    "farbe": "#FFEDD5"
+  },
+  {
+    "artikelnummer": "592",
+    "name": "Ware",
+    "beschreibung": "Light User \r\n\r\n•\tMicrosoft 365 Business Standard Account inklusive Mail-Archivie…",
+    "preis": 25.0,
+    "kategorie": "5 - Support-Verträge",
+    "farbe": "#FFEDD5"
+  },
+  {
+    "artikelnummer": "593",
+    "name": "Ware",
+    "beschreibung": "Mail only User \r\n\r\n•\tMicrosoft 365 Exchange Online Plan 1 Account inklusive Mail…",
+    "preis": 10.0,
+    "kategorie": "5 - Support-Verträge",
+    "farbe": "#FFEDD5"
+  },
+  {
+    "artikelnummer": "700",
+    "name": "Ware",
+    "beschreibung": "<b>.eu Domain</b>\r\n<ol>\r\n<li>DOMAINNAME.eu</li>\r\n</ol>\r\n\r\n<i>Laufzeit: 1 Jahr</i…",
+    "preis": 10.0,
+    "kategorie": "7 - Domains",
+    "farbe": "#FCE7F3"
+  },
+  {
+    "artikelnummer": "701",
+    "name": "Ware",
+    "beschreibung": "<b>.at Domain</b>\r\n<ol>\r\n<li>DOMAINNAME.at</li>\r\n</ol>\r\n\r\n<i>Laufzeit: 1 Jahr</i…",
+    "preis": 12.0,
+    "kategorie": "7 - Domains",
+    "farbe": "#FCE7F3"
+  },
+  {
+    "artikelnummer": "702",
+    "name": "Ware",
+    "beschreibung": "<b>.ch Domain</b>\r\n<ol>\r\n<li>DOMAINNAME.ch</li>\r\n</ol>\r\n\r\n<i>Laufzeit: 1 Jahr</i…",
+    "preis": 17.0,
+    "kategorie": "7 - Domains",
+    "farbe": "#FCE7F3"
+  },
+  {
+    "artikelnummer": "703",
+    "name": "Ware",
+    "beschreibung": "<b>.co Domain</b>\r\n<ol>\r\n<li>DOMAINNAME.co</li>\r\n</ol>\r\n\r\n<i>Laufzeit: 1 Jahr</i…",
+    "preis": 34.0,
+    "kategorie": "7 - Domains",
+    "farbe": "#FCE7F3"
+  },
+  {
+    "artikelnummer": "704",
+    "name": "Ware",
+    "beschreibung": "<b>.com Domain</b>\r\n<ol>\r\n<li>DOMAINNAME.com</li>\r\n</ol>\r\n\r\n<i>Laufzeit: 1 Jahr<…",
+    "preis": 13.0,
+    "kategorie": "7 - Domains",
+    "farbe": "#FCE7F3"
+  },
+  {
+    "artikelnummer": "705",
+    "name": "Ware",
+    "beschreibung": "<b>.eu Domain</b>\r\n<ol>\r\n<li>DOMAINNAME.eu</li>\r\n</ol>\r\n\r\n<i>Laufzeit: 1 Jahr</i…",
+    "preis": 3.0,
+    "kategorie": "7 - Domains",
+    "farbe": "#FCE7F3"
+  },
+  {
+    "artikelnummer": "706",
+    "name": "Ware",
+    "beschreibung": "<b>.fr Domain</b>\r\n<ol>\r\n<li>DOMAINNAME.fr</li>\r\n</ol>\r\n\r\n<i>Laufzeit: 1 Jahr</i…",
+    "preis": 14.5,
+    "kategorie": "7 - Domains",
+    "farbe": "#FCE7F3"
+  },
+  {
+    "artikelnummer": "707",
+    "name": "Ware",
+    "beschreibung": "<b>.info Domain</b>\r\n<ol>\r\n<li>DOMAINNAME.info</li>\r\n</ol>\r\n\r\n<i>Laufzeit: 1 Jah…",
+    "preis": 19.5,
+    "kategorie": "7 - Domains",
+    "farbe": "#FCE7F3"
+  },
+  {
+    "artikelnummer": "708",
+    "name": "Ware",
+    "beschreibung": "<b>.nl Domain</b>\r\n<ol>\r\n<li>DOMAINNAME.nl</li>\r\n</ol>\r\n\r\n<i>Laufzeit: 1 Jahr</i…",
+    "preis": 12.0,
+    "kategorie": "7 - Domains",
+    "farbe": "#FCE7F3"
+  },
+  {
+    "artikelnummer": "709",
+    "name": "Ware",
+    "beschreibung": "<b>.online Domain</b>\r\n<ol>\r\n<li>DOMAINNAME.online</li>\r\n</ol>\r\n\r\n<i>Laufzeit: 1…",
+    "preis": 42.0,
+    "kategorie": "7 - Domains",
+    "farbe": "#FCE7F3"
+  },
+  {
+    "artikelnummer": "710",
+    "name": "Ware",
+    "beschreibung": "<b>.shop Domain</b>\r\n<ol>\r\n<li>DOMAINNAME.shop</li>\r\n</ol>\r\n\r\n<i>Laufzeit: 1 Jah…",
+    "preis": 38.5,
+    "kategorie": "7 - Domains",
+    "farbe": "#FCE7F3"
+  },
+  {
+    "artikelnummer": "711",
+    "name": "Ware",
+    "beschreibung": "<b>.uk Domain</b>\r\n<ol>\r\n<li>DOMAINNAME.uk</li>\r\n</ol>\r\n\r\n<i>Laufzeit: 1 Jahr</i…",
+    "preis": 15.5,
+    "kategorie": "7 - Domains",
+    "farbe": "#FCE7F3"
+  },
+  {
+    "artikelnummer": "712",
+    "name": "Ware",
+    "beschreibung": "<b>.us Domain</b>\r\n<ol>\r\n<li>DOMAINNAME.us</li>\r\n</ol>\r\n\r\n<i>Laufzeit: 1 Jahr</i…",
+    "preis": 13.0,
+    "kategorie": "7 - Domains",
+    "farbe": "#FCE7F3"
+  },
+  {
+    "artikelnummer": "713",
+    "name": "Ware",
+    "beschreibung": "<b>.de Domain</b>\r\n<ol>\r\n<li>DOMAINNAME.de</li>\r\n</ol>\r\n\r\n<i>Laufzeit: 1 Jahr</i…",
+    "preis": 3.0,
+    "kategorie": "7 - Domains",
+    "farbe": "#FCE7F3"
+  },
+  {
+    "artikelnummer": "714",
+    "name": "Ware",
+    "beschreibung": "<b>.net Domain</b>\r\n<ol>\r\n<li>DOMAINNAMEnet</li>\r\n</ol>\r\n\r\n<i>Laufzeit: 1 Jahr</…",
+    "preis": 15.5,
+    "kategorie": "7 - Domains",
+    "farbe": "#FCE7F3"
+  }
+];
+// Simulation: Zeige nur Konsole
+console.log("Produkte geladen:", produkte);
